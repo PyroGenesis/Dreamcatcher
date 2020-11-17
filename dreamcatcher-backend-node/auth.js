bda9aaa5719438936b5b2700db4a2bce57b1bd20
Burhanuddin Lakdawala <blakdawa@uci.edu>
2020-11-16 21:57:34 -0800

    signup db update

--- dreamcatcher-backend-node/auth.js
+++ dreamcatcher-backend-node/auth.js
@@ -37,4 +37,51 @@ router.post('/', (req, res) => {
 });
 
 
+router.post('/signup', async (req, res) => {
+    const uid = req.body.uid;
+    const email = req.body.email;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const userName = req.body.userName;
+
+    if (uid == null || email == null || firstName == null || lastName == null || userName == null) {
+        res.statusCode = 400;
+        res.json({
+            status: res.statusCode,
+            message: 'Missing data',
+            data: null
+        });
+        return;
+    }
+
+    const userCreateOp = db.batch();
+
+    const userDoc = db.collection('users').doc(uid);
+    const usernameMapDoc = db.collection('usernameToDetails').doc(userName);
+
+    userCreateOp.set(userDoc, {
+        createdAt: new Date(),
+        email: email,
+        firstName: firstName,
+        lastName: lastName
+    });
+    userCreateOp.set(userDoc.collection('profile').doc('default'), {
+        education: {},
+        experience: {},
+        about: "This user hasn't shared anything about them yet"
+    });
+
+    userCreateOp.set(usernameMapDoc, {
+        uid: uid
+    });
+
+    await userCreateOp.commit();
+
+    res.statusCode = 200;
+    res.json({
+        status: res.statusCode,
+        message: 'success',
+        data: null
+    });
+});
 module.exports = router;
