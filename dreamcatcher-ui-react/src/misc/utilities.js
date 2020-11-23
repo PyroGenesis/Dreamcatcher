35741557ddfa9602cb21d918dd17e0e792e5ac9c
Burhanuddin Lakdawala <blakdawa@uci.edu>
2020-11-22 18:19:21 -0800

    info on how to use firebaseDateToJSDAte

--- dreamcatcher-ui-react/src/misc/utilities.js
+++ dreamcatcher-ui-react/src/misc/utilities.js
@@ -1,5 +1,14 @@
 module.exports = {
     firebaseDateToJSDate: (dateObj, options) => {
+        /**
+         * Example of options:
+         * {
+                month: 'short',
+                year: 'numeric'
+         * }
+         * More info here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat
+         */
+
         let milliseconds = 0;
         milliseconds += dateObj._seconds * 1000;
         milliseconds += Math.floor(dateObj._nanoseconds / 1000000);
