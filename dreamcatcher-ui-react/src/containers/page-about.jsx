bf999afaab07bdd0f159a8bbef92571096c95df7
Burhanuddin Lakdawala <blakdawa@uci.edu>
2020-12-05 21:34:56 -0800

    made about page responsive

--- dreamcatcher-ui-react/src/containers/page-about.jsx
+++ dreamcatcher-ui-react/src/containers/page-about.jsx
@@ -1,69 +1,75 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import video from '../videos/ad.mp4';
-import { Card, CardContent, Typography } from "@material-ui/core";
+import { Card, CardContent, Typography, withStyles } from "@material-ui/core";
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
-import user from '../images/user.JPG';
+import user from '../images/user.png';
+import burhan from '../images/burhan.jpg';
+
+const styles = theme => ({
+    root: {
+        backgroundColor: "red"
+    }
+});
 
 class AboutPage extends Component {
 
+    constructor(props) {
+        super(props);
+        console.log(props);
+    }
+
+
     render() {
-        return ( 
-            <div>
-                <br></br>
-                <Grid container align="center">
-                    <Grid item xs={12}>
-                        <video width="40%" controls>
+        const devs = [{
+            name: 'Anand Abhay Deshpande',
+            position: 'Full Stack Engineer',
+            image: user
+        }, {
+            name: 'Burhanuddin M. Lakdawala',
+            position: 'Backend Engineer and DBA',
+            image: burhan
+        }, {
+            name: 'Yash Kulkarni',
+            position: 'Full Stack Engineer',
+            image: user
+        }];
+
+        const { theme, classes } = this.props;
+        return (
+            <div className="body-content">
+                <Grid container direction="column" alignItems="center" spacing={1} style={{ height: '100%', width: '100%', padding: theme.spacing(1) }}>
+                    <div style={{ height: '50%', width: '100%', display: 'flex', justifyContent: 'center' }}>
+                        <video height="100%" controls>
                             <source src={video} type="video/mp4"></source>
                             Your browser does not support HTML video.
                         </video>
-                    </Grid>
-                </Grid>
-                <br></br>
-                <Grid container align="center" spacing={5}>
-                    <Grid item xs={4}>
-                        <Card  style={{margin: '20px', minHeight: '125px'}}>
-                        <Avatar alt="Remy Sharp" src={user} style = {{width:'50%', height:'50%'}} />
-                        <CardContent>
-                            <Typography variant="h4">
-                                Anand Abhay Deshpande
-                            </Typography>
-                            <Typography variant="h5">
-                                Full Stack Engineer
-                            </Typography>
-                        </CardContent>
-                        </Card>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Card style={{margin: '20px', minHeight: '125px'}} >
-                        <Avatar alt="Remy Sharp" src={user} style = {{width:'50%', height:'50%'}} />
-                        <CardContent>
-                            <Typography variant="h4">
-                                Burhanuddin Lakdawala
-                            </Typography>
-                            <Typography variant="h5">
-                                Backend Engineer & DBA
-                            </Typography>
-                        </CardContent>
-                        </Card>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Card style={{margin: '20px', minHeight: '125px'}} >
-                        <Avatar alt="Remy Sharp" src={user} style = {{width:'50%', height:'50%'}} />
-                        <CardContent>
-                            <Typography variant="h4">
-                                Yash Uday Kulkarni
-                            </Typography>
-                            <Typography variant="h5">
-                                Front End and Infrastructure Developer
-                            </Typography>
-                        </CardContent>
-                        </Card>
-                    </Grid>
+                    </div>
+                    <div className="row" style={{ height: '50%', width: '100%', paddingTop: 12}}>
+                    {/* <Grid item style={{ height: '50%', width: '100%', display: 'flex' }}> */}
+                        {devs.map(dev => (
+                            <Card className="col-12 col-lg-4" style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                                <div style={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                    <Avatar alt="Remy Sharp" src={dev.image} style={{ height: 200, width: 200 }}>
+                                    </Avatar>
+                                </div>
+
+                                <CardContent>
+                                    <Typography align="center" variant="h5">
+                                        {dev.name}
+                                    </Typography>
+                                    <Typography align="center" variant="h6">
+                                        {dev.position}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        ))}
+                    {/* </Grid> */}
+                    </div>
                 </Grid>
             </div>
         )
     }
 }
 
-export default AboutPage
+export default withStyles(styles, { withTheme: true })(AboutPage);
